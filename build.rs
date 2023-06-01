f0ebe08a6e9ea8b80a8e24b5c020a1989709b299
Miroslav Bajtoš <oss@bajtos.net>
2023-06-01 15:11:19 +0200

    chore: upgrade Go dependencies (#2)
    * chore: upgrade Go dependencies
    * ci: set location of go.sum file to enable caching
    * chore: rebuild golassie when go.sum changes
    
    ---------
    
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ use std::process::Command;
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=go-lib/go.sum");
     println!("cargo:rerun-if-changed=go-lib/lassie.go");
 
     build_lassie();
